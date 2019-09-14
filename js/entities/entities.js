4c6b988b0ea71f26f6a0678423899f0dee148160
TSAEit <tjm27@scasd.org>
2019-09-14 11:18:32 -0400

    Basic Stuff

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -9,13 +9,64 @@ game.PlayerEntity = me.Entity.extend({
     init:function (x, y, settings) {
         // call the constructor
         this._super(me.Entity, 'init', [x, y , settings]);
+        this.body.setVelocity(2, 2);
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+        this.alwaysUpdate = true;
+        
+        this.renderable.addAnimation("idle",  [1]);
+        
+        this.renderable.setCurrentAnimation("idle");
     },
 
     /**
      * update the entity
      */
     update : function (dt) {
+        var moving = false;
+        if (me.input.isKeyPressed('left')) {
+            moving = true;
+            // flip the sprite on horizontal axis
+            this.renderable.flipX(true);
 
+            // update the entity velocity
+            this.body.vel.x -= this.body.accel.x * me.timer.tick;
+            }
+        else if (me.input.isKeyPressed('right')) {
+            moving = true;
+            // unflip the sprite
+            this.renderable.flipX(false);
+
+            // update the entity velocity
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+        } else {
+              this.body.vel.x = 0;
+        }
+        if (me.input.isKeyPressed('up')) {
+            moving = true;
+
+            // update the entity velocity
+            this.body.vel.y -= this.body.accel.y * me.timer.tick;
+            }
+        else if (me.input.isKeyPressed('down')) {
+            moving = true;
+
+            // update the entity velocity
+            this.body.vel.y += this.body.accel.y * me.timer.tick;
+        } else {
+              this.body.vel.y = 0;
+        }
+        
+        /*
+        if (moving) {
+            if (!this.renderable.isCurrentAnimation("walk")) {
+                this.renderable.setCurrentAnimation("walk");
+            }
+        } else {
+            if (!this.renderable.isCurrentAnimation("stand")) {
+                this.renderable.setCurrentAnimation("stand");
+            }
+        }
+        */
         // apply physics to the body (this moves the entity)
         this.body.update(dt);
 
