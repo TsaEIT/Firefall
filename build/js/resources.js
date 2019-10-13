7c3da95bad9216ae51454a1b026c381bdda7d275
TSAEit <tjm27@scasd.org>
2019-10-13 13:14:19 -0400

    Trying to fix this

---
+++ build/js/resources.js
@@ -0,0 +1,92 @@
+game.resources = [
+    {
+        "name": "1 (2)",
+        "type": "image",
+        "src": "data/img/map/1 (2).png"
+    },
+    {
+        "name": "background",
+        "type": "image",
+        "src": "data/img/map/background.png"
+    },
+    {
+        "name": "background2",
+        "type": "image",
+        "src": "data/img/map/background2.png"
+    },
+    {
+        "name": "level1_tilet_img",
+        "type": "image",
+        "src": "data/img/map/level1_tilet_img.png"
+    },
+    {
+        "name": "monsters",
+        "type": "image",
+        "src": "data/img/monsters.png"
+    },
+    {
+        "name": "pinchers_1",
+        "type": "image",
+        "src": "data/img/pincers/pinchers_1.png"
+    },
+    {
+        "name": "pinchers_2",
+        "type": "image",
+        "src": "data/img/pincers/pinchers_2.png"
+    },
+    {
+        "name": "pinchers_3",
+        "type": "image",
+        "src": "data/img/pincers/pinchers_3.png"
+    },
+    {
+        "name": "pinchers_4",
+        "type": "image",
+        "src": "data/img/pincers/pinchers_4.png"
+    },
+    {
+        "name": "pinchers_5",
+        "type": "image",
+        "src": "data/img/pincers/pinchers_5.png"
+    },
+    {
+        "name": "pinchers_6",
+        "type": "image",
+        "src": "data/img/pincers/pinchers_6.png"
+    },
+    {
+        "name": "pinchers_small",
+        "type": "image",
+        "src": "data/img/pincers/pinchers_small.png"
+    },
+    {
+        "name": "pinchers_small (1)",
+        "type": "image",
+        "src": "data/img/pinchers_small (1).png"
+    },
+    {
+        "name": "pinchers_small",
+        "type": "image",
+        "src": "data/img/pinchers_small.png"
+    },
+    {
+        "name": "pinchers",
+        "type": "image",
+        "src": "data/img/pinchers.png"
+    },
+    {
+        "name": "player",
+        "type": "image",
+        "src": "data/img/sprites/player.png"
+    },
+    {
+        "name": "level1",
+        "type": "tmx",
+        "src": "data/map/level1.tmx"
+    },
+    {
+        "name": "level1_tileset",
+        "type": "tsx",
+        "src": "data/map/level1_tileset.tsx"
+    }
+];
