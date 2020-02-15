3bab39ad92e951fca3d1eb0db871305d4a835252
19UV <the19UV@gmail.com>
2020-02-15 13:19:51 -0500

    Added Boss Level Skeleton

---
+++ data/map/boss_tileset_thing.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="boss_tileset_thing" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="../img/map/boss_tileset.png" width="640" height="640"/>
+</tileset>
